bb0ad8ea8d41bd26c7335ce95c02455282133bcc
huiji <fmti0720@gmail.com>
2022-01-28 18:11:45 +0900

    스프링 핵심 원리 이해2 - 객체 지향 원리 적용

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,41 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FicDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//실제동작에 필요한 구현객체 생성
+//생성한 객체 인스턴스의 참조를 통해서 생성자를 주입해준다.
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+    @Bean
+    public MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(getMemberRepository(), new FicDiscountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+
+}
